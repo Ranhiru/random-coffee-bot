03fc622d502a239eb6e3b20c6ddc01723fb0037e
Jude Cooray <ranhiru@gmail.com>
2021-04-05 21:01:21 +1000

    Call the postRandomCoffeePairMessageToChannels method from the main method

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-const { App } = require('@slack/bolt');
-
-// Initializes your app with your bot token and signing secret
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
+const { postRandomCoffeePairMessageToChannels } = require('./coffee_pair_generator.js');
 
 const postMessageToChannel = async () => {
   try {
-    const result = await app.client.chat.postMessage({
-      channel: "random",
-      text: "Hello from Random Coffee Bot!",
-      token: process.env.SLACK_BOT_TOKEN
-    });
-
-    console.log(result);
+    postRandomCoffeePairMessageToChannels();
   } catch (error) {
     console.error(error);
   }
