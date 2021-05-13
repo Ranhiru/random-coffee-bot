14245910a14589ed20f116374be15ae3f4fe2068
Jude Cooray <ranhiru@gmail.com>
2021-05-13 21:02:45 +1000

    Fixed chunking with uneven group sizes

--- __tests__/coffee_pair_generator_spec.js
+++ __tests__/coffee_pair_generator_spec.js
@@ -1,4 +1,4 @@
-const { generateMessageForGroup, generateRandomCoffeePairMessage } = require("../lib/coffee_pair_generator.js");
+const { generateRandomCoffeePairMessage, chunkMembers } = require("../lib/coffee_pair_generator.js");
 
 describe("generateRandomCoffeePairMessage", () => {
   it("will generate a message with the pairs", () => {
@@ -14,3 +14,28 @@ describe("generateRandomCoffeePairMessage", () => {
     expect(coffeePairMessage).toContain(`2. <@Mary> and <@Mark>`);
   });
 });
+
+describe("convertMemberListToPairs", function() {
+  it("will create chunks with equal numbers number of members", function() {
+    const members = [ "John", "Craig", "Mary", "Mark"];
+
+    const membersPerGroup = 2;
+    const pairs = chunkMembers(members, membersPerGroup);
+    expect(pairs.length).toEqual(2)
+
+    expect(pairs[0]).toEqual(["John", "Craig"])
+    expect(pairs[1]).toEqual(["Mary", "Mark"])
+  });
+
+  it("will combine members with the last group if the chunks are not the same size", function() {
+    const members = [ "John", "Craig", "Mary", "Mark", "Duncan", "Stu", "Brett"];
+
+    const membersPerGroup = 3;
+    const pairs = chunkMembers(members, membersPerGroup);
+
+    expect(pairs.length).toEqual(2)
+
+    expect(pairs[0]).toEqual(["John", "Craig", "Mary"])
+    expect(pairs[1]).toEqual(["Mark", "Duncan", "Stu", "Brett"])
+  });
+})
